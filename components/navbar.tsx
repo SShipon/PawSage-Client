776e1b9e4c59447c1be44ec4b767d273269ce25c
SShipon <nxshipon@gmail.com>
2024-10-01 19:06:39 -0700

    bug fiex navbar add

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -42,7 +42,36 @@ export const Navbar = () => {
       position="sticky"
       className="dark:bg-slate-900 bg-primary"
     >
-     
+      <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
+        <NavbarBrand as="li" className="gap-3 max-w-fit">
+          <NextLink className="flex justify-start items-center gap-1" href="/">
+            <Image
+              className="dark:invert"
+              src={logo}
+              alt="logo"
+              width={32}
+              height={32}
+            />
+            <p className="font-bold text-inherit">PawSage</p>
+          </NextLink>
+        </NavbarBrand>
+        <ul className="hidden lg:flex gap-4 justify-start ml-2">
+          {navItems.map((item) => (
+            <NavbarItem key={item.href}>
+              <NextLink
+                className={clsx(
+                  linkStyles({ color: "foreground" }),
+                  "data-[active=true]:text-primary data-[active=true]:font-medium"
+                )}
+                color="foreground"
+                href={item.href}
+              >
+                {item.label}
+              </NextLink>
+            </NavbarItem>
+          ))}
+        </ul>
+      </NavbarContent>
 
       <NavbarContent className="hidden sm:flex basis-1/5 sm:basis-full" justify="end">
         <NavbarItem className="hidden sm:flex gap-2">
