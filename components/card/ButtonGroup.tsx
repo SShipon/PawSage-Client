3d4251facae58b7fd75756fa5d741982ae0d6bb5
SShipon <nxshipon@gmail.com>
2024-10-01 18:52:40 -0700

    next ui and card components add

---
+++ components/card/ButtonGroup.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import React from "react";
+import { Button } from "@nextui-org/button";
+import { ArrowDown, ArrowUp, Share } from "lucide-react";
+import { useDownVote, useUpVote } from "@/hooks/vote.hook";
+
+const ButtonGroup = ({
+  postId,
+  userId,
+}: {
+  postId: string | undefined;
+  userId: string | undefined;
+}) => {
+  const { mutate: addVote, data } = useUpVote();
+
+  const { mutate: addDownVote } = useDownVote();
+  const handleUpVote = () => {
+    addVote({ postId, userId, type: "upvote" });
+  };
+
+  const handleDownVote = () => {
+    addDownVote({ postId, userId, type: "downvote" });
+  };
+  return (
+    <div className="flex my-2">
+      <Button
+        onClick={handleUpVote}
+        startContent={<ArrowUp />}
+        variant="light"
+        className="w-1/3"
+      >
+        Up Vote
+      </Button>
+      <Button
+        onClick={handleDownVote}
+        startContent={<ArrowDown />}
+        variant="light"
+        className="w-1/3"
+      >
+        Down Vote
+      </Button>
+      <Button startContent={<Share />} variant="light" className="w-1/3">
+        Share
+      </Button>
+    </div>
+  );
+};
+
+export default ButtonGroup;
