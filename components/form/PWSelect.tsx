e8e4ad93bbc240a87a8e3163b4d1fbbd5f5fbd59
SShipon <nxshipon@gmail.com>
2024-10-01 19:01:00 -0700

    solved the singUP page error

---
+++ components/form/PWSelect.tsx
@@ -0,0 +1,41 @@
+import { IInput } from "@/types";
+import { Select, SelectItem } from "@nextui-org/select";
+import React from "react";
+import { useFormContext } from "react-hook-form";
+
+interface IProps extends IInput {
+  options: {
+    key: string;
+    label: string;
+  }[];
+}
+
+const PWSelect = ({
+  variant = "bordered",
+  name,
+  label,
+  options,
+  isDisabled,
+  defaultValue,
+}: IProps) => {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+  return (
+    <Select
+      defaultSelectedKeys={defaultValue}
+      errorMessage={errors[name] ? (errors[name]?.message as string) : ""}
+      variant={variant}
+      label={label}
+      {...register(name)}
+      isDisabled={isDisabled}
+    >
+      {options.map((option, index) => (
+        <SelectItem key={option.key}>{option.label}</SelectItem>
+      ))}
+    </Select>
+  );
+};
+
+export default PWSelect;
